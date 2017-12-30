b8a581f7a72911fcdd9f54dc1005f8bc15ee2514
Rahul Chanila <rahulcssjce@gmail.com>
2017-12-30 21:02:50 +1100

    add gradient to nav-header

--- src/components/nav-header.js
+++ src/components/nav-header.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 const Header = styled.div`
+  background-image: linear-gradient(firebrick, brown);
+  box-shadow: 0 1px 2px #ac1d1c;
   margin-top: 20px;
   text-align: center;
+  text-shadow: 0 1px 2px #ac1d1c;
   padding: 5px;
-  background: brown;
   @media (min-width: 420px) {
     margin: 10px 20%;
     text-align: right;
