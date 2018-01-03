c1e7040a39fd188a4e5f8230fe396b9754452828
pkodmad <pkodmad@atlassian.com>
2018-01-03 16:34:11 +1100

    Add the right babel preset env

--- plugins/gatsby-plugin-medium-publication/gatsby-node.js
+++ plugins/gatsby-plugin-medium-publication/gatsby-node.js
@@ -1,9 +1,14 @@
-const load = require("./rss-to-json");
-const crypto = require("crypto");
-const kebabCase = require("lodash/kebabCase");
-const Promise = require("bluebird");
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } } function _next(value) { step("next", value); } function _throw(err) { step("throw", err); } _next(); }); }; }
 
-const loadRss = Promise.promisify(load);
+const load = require("./rss-to-json");
+
+const crypto = require("crypto");
+
+const kebabCase = require("lodash/kebabCase");
+
+const _Promise = require("bluebird");
+
+const loadRss = _Promise.promisify(load);
 
 const processDatum = item => {
   const digest = crypto.createHash(`md5`).update(JSON.stringify(item)).digest(`hex`);
@@ -23,14 +28,24 @@ const processDatum = item => {
   return data;
 };
 
-exports.sourceNodes = async ({ boundActionCreators }) => {
-  const { createNode } = boundActionCreators;
-  const rss = await loadRss("https://medium.com/feed/in-my-time-of-living");
-
-  rss.items.forEach(item => {
-    console.log("creating node");
-    createNode(processDatum(item));
+exports.sourceNodes =
+/*#__PURE__*/
+function () {
+  var _ref = _asyncToGenerator(function* ({
+    boundActionCreators
+  }) {
+    const {
+      createNode
+    } = boundActionCreators;
+    const rss = yield loadRss("https://medium.com/feed/in-my-time-of-living");
+    rss.items.forEach(item => {
+      console.log("creating node");
+      createNode(processDatum(item));
+    });
+    return;
   });
 
-  return;
-};
+  return function (_x) {
+    return _ref.apply(this, arguments);
+  };
+}();
