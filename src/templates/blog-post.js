db37db90932c2894e88d6ee6a0dfd29fcb4e024c
Pavithra Kodmad <pksjce@gmail.com>
2018-01-28 14:10:41 +1100

    Make a neater blogpost

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,22 +1,30 @@
 import React from "react";
 import Helmet from "react-helmet";
 import Header from "../components/header";
-import Paragraph, { SectionWrapper } from "../components/paragraph";
 import styled from "styled-components";
 
+const PostParagraph = styled.div`
+background-color: white;
+padding: 5px 40px;
+font-size: 14px;
+font-family: serif;
+line-height: 20px;
+`
+
 const Date = styled.div`
-  font-size: 0.9em;
-  font-family: "Ranga", cursive;
+  font-size: 0.7em;
+  text-align: right;
+  margin-bottom: 5px;
 `;
 
 export default function Template(props) {
   const { markdownRemark: post } = props.data;
   return (
-    <Paragraph>
-      <Header fontSize={"1.6em"}>{post.frontmatter.title}</Header>
+    <PostParagraph>
+      <Header fontSize={"1.3em"}>{post.frontmatter.title}</Header>
       <Date>{post.frontmatter.date}</Date>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </Paragraph>
+    </PostParagraph>
   );
 }
 
